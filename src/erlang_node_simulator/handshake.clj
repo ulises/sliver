bd34c47a8e39c3a497f70555e13937632461b58c
Ulises <ulises.cervino@gmail.com>
2014-11-13 08:15:56 +0000

    recv_status = ok

--- src/erlang_node_simulator/handshake.clj
+++ src/erlang_node_simulator/handshake.clj
@@ -1,5 +1,7 @@
 (ns erlang-node-simulator.handshake
-  (:require [erlang-node-simulator.util :as util]))
+  (:require [bytebuffer.buff :refer [take-short take-ubyte]]
+            [erlang-node-simulator.util :as util])
+  (:import [java.nio ByteBuffer]))
 
 (defn send-name [^String name]
   (let [bytes (concat [(byte \n) 0 5 0 3 0x7f 0xfd]
@@ -8,3 +10,10 @@
     (util/flip-pack (+ 2 len)
                     (str "s" (apply str (repeat len "b")))
                     (concat [len] bytes))))
+
+(defn recv-status
+  [^ByteBuffer payload]
+  ;; packet size - 1 byte for the tag = length of the status
+  (let [len (dec (take-short payload))]
+    (when (= \s (char (take-ubyte payload)))
+      (keyword (apply str (map char (repeatedly len #(take-ubyte payload))))))))
--- test/erlang_node_simulator/handshake_test.clj
+++ test/erlang_node_simulator/handshake_test.clj
@@ -10,6 +10,10 @@
     (h/bb-is-= (send-name "bar@127.0.0.1")
                (h/file->bb "send_name.bin"))))
 
+(deftest test-recv-status-ok
+  (testing "recv status ok"
+    (is (= :ok (recv-status (h/file->bb "recv_status_ok.bin"))))))
+
 
 
 
