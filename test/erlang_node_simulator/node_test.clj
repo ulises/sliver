d6ae9b55ce9e0d39ba8b38a41bf3cbebe935b7c5
Ulises <ulises.cervino@gmail.com>
2014-11-18 00:32:03 +0000

    Move helper fns to the right namespace.

--- test/erlang_node_simulator/node_test.clj
+++ test/erlang_node_simulator/node_test.clj
@@ -1,38 +1,22 @@
 (ns erlang-node-simulator.node-test
-  (:require [clojure.java.shell :refer [sh]]
-            [clojure.test :refer :all]
-            [erlang-node-simulator.node :refer :all]))
-
-(defn- erl [name cookie]
-  (future (sh "erl" "-name" name "-setcookie" cookie "-noshell")))
-
-(defn- epmd [arg]
-  (sh "epmd" arg))
-
-(defn- epmd-port [name]
-  (let [o          (:out (sh "epmd" "-names"))
-        match      (re-find (re-pattern (format "name %s at port (\\d+)" name)) o)
-        maybe-port (second match)]
-    (assert maybe-port)
-    (Integer/parseInt maybe-port)))
-
-(defn- killall [name]
-  (sh "killall" name))
+  (:require [clojure.test :refer :all]
+            [erlang-node-simulator.node :refer :all]
+            [erlang-node-simulator.test-helpers :as h]))
 
 (defn- epmd-erl-fixture [f]
-  (epmd "-daemon -relaxed_command_check")
-  (erl "foo@127.0.0.1" "monster")
+  (h/epmd "-daemon" "-relaxed_command_check")
+  (h/erl "foo@127.0.0.1" "monster")
 
   (Thread/sleep 1000)
 
   (f)
 
-  (killall "beam.smp")
-  (epmd "-kill"))
+  (h/killall "beam.smp")
+  (h/epmd "-kill"))
 
 (use-fixtures :each epmd-erl-fixture)
 
 (deftest test-node-connect
   (let [node (node "bar@127.0.0.1" "monster")]
     (is (= :ok (connect node {:host "127.0.0.1"
-                              :port (epmd-port "foo")})))))
+                              :port (h/epmd-port "foo")})))))
--- test/erlang_node_simulator/test_helpers.clj
+++ test/erlang_node_simulator/test_helpers.clj
@@ -1,6 +1,7 @@
 (ns erlang-node-simulator.test-helpers
   (:require [bytebuffer.buff :refer [take-ubyte]]
             [clojure.java.io :as io]
+            [clojure.java.shell :refer [sh]]
             [clojure.test :refer [is]])
   (:import [java.nio ByteBuffer]))
 
@@ -12,3 +13,19 @@
 
 (defn bytes-seq [bb]
   (repeatedly (.remaining bb) #(format "%x" (take-ubyte bb))))
+
+(defn erl [name cookie]
+  (future (sh "erl" "-name" name "-setcookie" cookie "-noshell")))
+
+(defn epmd [& args]
+  (apply sh (conj args "epmd")))
+
+(defn epmd-port [name]
+  (let [o          (:out (sh "epmd" "-names"))
+        match      (re-find (re-pattern (format "name %s at port (\\d+)" name)) o)
+        maybe-port (second match)]
+    (assert maybe-port)
+    (Integer/parseInt maybe-port)))
+
+(defn killall [name]
+  (sh "killall" name))
