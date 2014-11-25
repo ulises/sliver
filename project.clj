75ee01811b5d67fc1b1bb5f19fbd81cfa7dc446a
Ulises <ulises.cervino@gmail.com>
2014-11-25 20:14:34 +0000

    borges

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [bytebuffer "0.2.0"]
-                 [com.taoensso/timbre "3.3.1"]]
+                 [com.taoensso/timbre "3.3.1"]
+                 [borges "0.1.1"]]
 
   :global-vars {*warn-on-reflection* true})
