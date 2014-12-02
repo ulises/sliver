a35d41bd659cc5db1725a08a5b42a83a89bbcf24
Ulises <ulises.cervino@gmail.com>
2014-12-02 15:11:59 +0000

    Connect to nodes on demand.

--- src/sliver/node.clj
+++ src/sliver/node.clj
@@ -26,10 +26,9 @@
   (pid [node]
     "Creates a new pid."))
 
-(defrecord Node [node-name cookie state pid-tracker]
+(defrecord Node [node-name cookie handlers state pid-tracker]
   NodeP
-  (connect [node other-node] (connect node other-node nil))
-  (connect [node other-node handlers]
+  (connect [node other-node]
     (let [[ack connection] (h/shake-hands node other-node)]
       (if (= :ok ack)
         (do (swap! state update-in [other-node] assoc :connection connection)
@@ -53,21 +52,30 @@
   ;; pid(0, 42, 0) ! message
   (send-message [node pid message]
     (let [other-node-name (second (re-find #"(\w+)@" (name (:node pid))))
-          connection (get-in @state [{:node-name other-node-name} :connection])]
+          other-node      {:node-name other-node-name}
+          connection      (get-in @state [other-node :connection])]
       (if connection
         (p/send-message connection pid message)
-        (timbre/info
-         (format "Couldn't find connection for %s - %s"
-                 other-node-name @state)))))
+        (do (timbre/info
+             (format "Couldn't find connection for %s. Connecting..."
+                     other-node-name))
+            ;; if the connection fails, this is will go into a crazy
+            ;; reconnect-fail DOS loop
+            (connect node other-node)
+            (recur pid message)))))
 
   ;; equivalent to {to, 'name@host'} ! message
   (send-registered-message [node from to other-node message]
     (let [connection (get-in @state [other-node :connection])]
       (if connection
         (p/send-reg-message connection from to message)
-        (timbre/info
-         (format "Couldn't find connection for %s - %s"
-                 other-node @state)))))
+        (do (timbre/info
+             (format "Couldn't find connection for %s. Connecting..."
+                     other-node))
+            ;; if the connection fails, this is will go into a crazy
+            ;; reconnect-fail DOS loop
+            (connect node other-node)
+            (recur from to other-node message)))))
 
   ;; creates a new pid. This is internal, and is likely to be used in
   ;; conjunction with some form of custom spawn implementation
@@ -88,5 +96,6 @@
          (alter pid-tracker assoc :pid next-pid :serial next-serial)
          new-pid)))))
 
-(defn node [name cookie]
-  (Node. name cookie (atom {}) (ref {:pid 0 :serial 0 :creation 0})))
+(defn node [name cookie & handlers]
+  (Node. name cookie handlers (atom {})
+         (ref {:pid 0 :serial 0 :creation 0})))
--- test/sliver/end_to_end_test.clj
+++ test/sliver/end_to_end_test.clj
@@ -12,9 +12,22 @@
   (let [message-received (promise)
         _                (h/escript "resources/echo-server.escript")
         other-node       {:node-name "foo"}
-        node             (n/connect (n/node "bar@127.0.0.1" "monster")
-                                    other-node
-                                    [(handler message-received)])
+        node             (n/connect (n/node "bar@127.0.0.1" "monster"
+                                            (handler message-received))
+                                    other-node)
+        pid              (n/pid node)
+        message          'ohai2u]
+    (n/send-registered-message node pid 'echo other-node
+                               [pid message])
+
+    (is (= @message-received message))))
+
+(deftest test-on-demand-ping-pong
+  (let [message-received (promise)
+        _                (h/escript "resources/echo-server.escript")
+        other-node       {:node-name "foo"}
+        node             (n/node "bar@127.0.0.1" "monster"
+                                 (handler message-received))
         pid              (n/pid node)
         message          'ohai2u]
     (n/send-registered-message node pid 'echo other-node
