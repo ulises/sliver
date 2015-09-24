6e265aabe2032e9564efd72b677795d7fddab4f9
Ulises Cervino Beresi <ulises.cervino@datastax.com>
2015-09-24 13:09:43 -0700

    Changes to example code to simply repl testing.

--- src/sliver/core.clj
+++ src/sliver/core.clj
@@ -4,11 +4,19 @@
 
 (def node (atom nil))
 
-(defn log-handler [node msg]
-  (log/info (reduce str msg)))
+(defn log-handler [node from to msg]
+  (try
+    (let [gen-call (first msg)
+          [pid reference] (second msg)
+          [_call module f args] (last msg)
+          result (apply (resolve (symbol (str module "/" f))) args)]
+      (n/send-message node pid [reference result]))
+    (catch Exception e
+      (log/info e))))
 
 (defn run []
   (when @node
     (n/stop @node))
-  (reset! node (n/node "bar@127.0.0.1" "monster" [log-handler]))
-  (n/start @node))
+  (reset! node (n/node "bar@127.0.0.1" "monster" [#'log-handler]))
+  (n/start @node)
+  (n/connect @node {:node-name "foo"}))
