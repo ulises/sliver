4a953adf172af5552c2862416672a0d13e7d0001
Ulises <ulises.cervino@gmail.com>
2015-11-14 10:49:11 +0000

    Clean up core with a simple example.

--- src/sliver/core.clj
+++ src/sliver/core.clj
@@ -6,18 +6,12 @@
 (def node (atom nil))
 
 (defn log-handler [node from to msg]
-  (try
-    (let [gen-call (first msg)
-          [pid reference] (second msg)
-          [_call module f args] (last msg)
-          result (apply (resolve (symbol (str module "/" f))) args)]
-      (ni/send-message node pid [reference result]))
-    (catch Exception e
-      (log/info e))))
+  (log/debug "FROM:" from)
+  (log/debug "TO:" to)
+  (log/debug "MSG:" msg))
 
 (defn run []
   (when @node
     (ni/stop @node))
   (reset! node (n/node "bar@127.0.0.1" "monster" [#'log-handler]))
-  (ni/start @node)
-  (ni/connect @node {:node-name "foo"}))
+  (ni/start @node))
